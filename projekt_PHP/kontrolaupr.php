b5a5749ffa0977a6adf2e7cdeb0020065cb9232e
Seremontis <37476307+Seremontis@users.noreply.github.com>
2018-05-28 19:14:19 +0200

    Add files via upload

---
+++ projekt_PHP/kontrolaupr.php
@@ -0,0 +1,11 @@
+<?php 
+function kontrolaDostepu(){
+    if(isset($_SESSION["zalogowany"])==false || empty($_SESSION["zalogowany"])==true || $_SESSION["zalogowany"]!="Pracownik"){
+        echo "<script>alert('Nie ma uprawnień do tego miejsca, zaloguj się');
+        window.location.href = 'index1.php';</script>";
+        exit;
+    
+    }
+}
+
+?>
